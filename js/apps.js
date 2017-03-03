fc6775e10ee5dc983d7176b4c6a67d2fe66021da
TaranAlbury242 <taranalbury@hotmail.com>
2017-03-02 20:53:48 -0500

    My first commit
    first commit of interactive photo gallery

---
+++ js/apps.js
@@ -0,0 +1,14 @@
+
+
+$('a[data-imagelightbox="demo"]').imageLightbox({
+arrows:true,
+button:true,
+overlay:true,
+caption:true
+});
+
+
+var filterizd = $('.content-photos').filterizr({
+
+                
+});
